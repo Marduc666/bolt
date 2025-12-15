eaddfa6385fec50a69ea5083f2f132e4e04bceda
Anders Matsson <109043445+Marduc666@users.noreply.github.com>
2025-12-15 19:30:57 +0100

    Fix .env.local check and read bindings

--- bindings.sh
+++ bindings.sh
@@ -2,14 +2,17 @@
 
 bindings=""
 
-while IFS= read -r line || [ -n "$line" ]; do
-  if [[ ! "$line" =~ ^# ]] && [[ -n "$line" ]]; then
-    name=$(echo "$line" | cut -d '=' -f 1)
-    value=$(echo "$line" | cut -d '=' -f 2-)
-    value=$(echo $value | sed 's/^"\(.*\)"$/\1/')
-    bindings+="--binding ${name}=${value} "
-  fi
-done < .env.local
+# Check if .env.local exists
+if [ -f . env.local ]; then
+  while IFS= read -r line || [ -n "$line" ]; do
+    if [[ !  "$line" =~ ^# ]] && [[ -n "$line" ]]; then
+      name=$(echo "$line" | cut -d '=' -f 1)
+      value=$(echo "$line" | cut -d '=' -f 2-)
+      value=$(echo $value | sed 's/^"\(.*\)"$/\1/')
+      bindings+="--binding ${name}=${value} "
+    fi
+  done < .env.local
+fi
 
 bindings=$(echo $bindings | sed 's/[[:space:]]*$//')
 
